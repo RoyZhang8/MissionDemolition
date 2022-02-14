20494b0ded1e08bf4c1529cfb80232519a737c1f
375247328 <375247328@qq.com>
2022-02-14 15:14:26 -0600

    update

--- MissionDemolition-Unity/Assets/Scripts/FollowCam.cs
+++ MissionDemolition-Unity/Assets/Scripts/FollowCam.cs
@@ -8,6 +8,9 @@ public class FollowCam : MonoBehaviour
     /****VARIABLES****/
     static public GameObject POI; //THE STATIC POINT OF INTEREST
 
+    [Header("Set in Inespector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
 
     [Header("Set Dynamically")]
     public float camZ;//desired Z pos of the camera
@@ -24,7 +27,16 @@ public class FollowCam : MonoBehaviour
 
         Vector3 destination = POI.transform.position;
 
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        //interpolate from current position to desition
+        destination = Vector3.Lerp(transform.position, destination, easing);
 
+        destination.z = camZ;
+
+        transform.position = destination;
+
+        Camera.main.orthographicSize = destination.y + 10; 
     }
 
     // Start is called before the first frame update
