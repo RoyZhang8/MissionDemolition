8e4635ca92c10285edfc5510345a7812508e2840
375247328 <375247328@qq.com>
2022-02-14 14:28:52 -0600

    123

---
+++ MissionDemolition-Unity/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+
+    /****VARIABLES****/
+    static public GameObject POI; //THE STATIC POINT OF INTEREST
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
